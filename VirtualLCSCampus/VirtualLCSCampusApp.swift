95b04924b1464a815f7cb2fe0a117cbf64ef1b4e
Steven Zhang <YZhang@lcs.on.ca>
2021-03-02 14:47:02 -0500

    Initial Commit

---
+++ VirtualLCSCampus/VirtualLCSCampusApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualLCSCampusApp.swift
+//  VirtualLCSCampus
+//
+//  Created by Steven Zhang on 2021-03-02.
+//
+
+import SwiftUI
+
+@main
+struct VirtualLCSCampusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
